7ae3d6e3ec0434969599cc6eb4aaaa018a0e3c4c
Timothy King <tmk@lordzork.com>
2017-06-27 11:35:40 -0400

    Modifies the redirect_to_finish_wizard method

--- lib/nfg_onboarder/onboarding_controller_helper.rb
+++ lib/nfg_onboarder/onboarding_controller_helper.rb
@@ -50,7 +50,7 @@ module NfgOnboarder
       private
 
       def redirect_to_finish_wizard(options)
-        redirect_to(finish_wizard_path + "?from_wicked_finish=true", options)
+        redirect_to(finish_wizard_path(options.merge(from_wicked_finish: true)))
       end
 
       def maybe_jump_to_last_visited_step
