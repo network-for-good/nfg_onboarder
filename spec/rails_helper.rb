1db84d0c0a0fb9f8b96eb6a951b2ddf9daf5c5e8
Timothy King <tmk@lordzork.com>
2018-12-17 16:45:37 -0500

    Require shoulda/matchers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,7 @@
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require 'rspec/rails'
 require 'factory_bot_rails'
+require 'shoulda/matchers'
 
 Rails.backtrace_cleaner.remove_silencers!
 ActiveRecord::Migrator.migrations_paths = 'spec/dummy/db/migrate'
