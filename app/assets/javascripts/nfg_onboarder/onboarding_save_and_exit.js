b2fd350830c3e9306c4a889d0797fdc470f4fd93
Andrew Porterfield <ajporterfield@gmail.com>
2019-08-30 10:26:10 -0400

    Replace const/let with var

--- app/assets/javascripts/nfg_onboarder/onboarding_save_and_exit.js
+++ app/assets/javascripts/nfg_onboarder/onboarding_save_and_exit.js
@@ -2,13 +2,13 @@ if(typeof NfgOnboarder == 'undefined') { // this checks for null and undefined
   window.NfgOnboarder = {};
 
   NfgOnboarder.submitOnboardingForm = function (form) {
-    const action = form.attr("action");
+    var action = form.attr("action");
     form.attr("action", action + "?exit=true");
     form.submit();
   };
 
   NfgOnboarder.submitOnboardingExitForm = function () {
-    let form = $('form#onboarding_main_form');
+    var form = $('form#onboarding_main_form');
     NfgOnboarder.submitOnboardingForm(form)
   };
 }
