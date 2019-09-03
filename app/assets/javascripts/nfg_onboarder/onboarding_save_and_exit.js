84a5b7d0e7aad37b5eecccc3915c864e1f578afc
Harsh <pathakh@msn.com>
2019-09-03 15:49:37 -0400

    Add exit methods to the generator and combine two js methods into one

--- app/assets/javascripts/nfg_onboarder/onboarding_save_and_exit.js
+++ app/assets/javascripts/nfg_onboarder/onboarding_save_and_exit.js
@@ -1,14 +1,10 @@
 if(typeof NfgOnboarder == 'undefined') { // this checks for null and undefined
   window.NfgOnboarder = {};
 
-  NfgOnboarder.submitOnboardingForm = function (form) {
-    var action = form.attr("action");
+  NfgOnboarder.submitOnboardingExitForm = function () {
+    let form = $('form#onboarding_main_form');
+    const action = form.attr("action");
     form.attr("action", action + "?exit=true");
     form.submit();
-  };
-
-  NfgOnboarder.submitOnboardingExitForm = function () {
-    var form = $('form#onboarding_main_form');
-    NfgOnboarder.submitOnboardingForm(form)
   };
 }
