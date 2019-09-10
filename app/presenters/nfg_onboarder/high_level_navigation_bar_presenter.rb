f35d7bb9cc6b1680ad8632c0cfde6e6254096d73
Jonathan Roehm <jonathan.roehm@networkforgood.com>
2019-09-10 16:27:12 -0400

    alphabetizes

--- app/presenters/nfg_onboarder/high_level_navigation_bar_presenter.rb
+++ app/presenters/nfg_onboarder/high_level_navigation_bar_presenter.rb
@@ -1,27 +1,15 @@
 module NfgOnboarder
   # Ex usage on a view: NfgOnboarder::HighLevelNavigationBarPresenter.new(onboarding_session, self)
   class HighLevelNavigationBarPresenter < NfgOnboarder::GemPresenter
-    # Returns an nfg_ui friendly status for the :step component's status trait
-    #
-    # Example usage:
-    # = ui.nfg :step, onboarder_presenter.step_status(the_step), step: 1, href: wizard_path(the_step)
-    def step_status(step)
-      return :active if step.to_sym == active_step.to_sym
-      return :visited if try(:completed_steps, h.controller_name).try(:include?, step)
-      # in case steps change, if the step or active step can't be found in what
-      # is currently the list of steps (based on the file origination type) don't barf
-      return :disabled if (all_steps.index(step.to_sym) || 0) > (all_steps.index(active_step.to_sym) || 0)
-    end
-
-    def step_icon(step)
-      step == all_steps.last ? 'check' : nil
-    end
-
     # Ensure that the href is nil (thus supporting accessibility via the nfg_ui Step)
     # when the step is disabled / unclickable
     # or on the last step, all links should have a nil :href
     def href(step, path: '')
       h.before_last_visited_point_of_no_return?(step) ? nil : path
+    end
+
+    def points_of_no_return
+      @points_of_no_return ||= h.controller.send(:points_of_no_return)
     end
 
     # set this to false on your own presenter if you would like to hide
@@ -32,8 +20,20 @@ module NfgOnboarder
       true
     end
 
-    def points_of_no_return
-      @points_of_no_return ||= h.controller.send(:points_of_no_return)
+    def step_icon(step)
+      step == all_steps.last ? 'check' : nil
+    end
+
+    # Returns an nfg_ui friendly status for the :step component's status trait
+    #
+    # Example usage:
+    # = ui.nfg :step, onboarder_presenter.step_status(the_step), step: 1, href: wizard_path(the_step)
+    def step_status(step)
+      return :active if step.to_sym == active_step.to_sym
+      return :visited if try(:completed_steps, h.controller_name).try(:include?, step)
+      # in case steps change, if the step or active step can't be found in what
+      # is currently the list of steps  don't barf
+      return :disabled if (all_steps.index(step.to_sym) || 0) > (all_steps.index(active_step.to_sym) || 0)
     end
   end
 end
