0de027b7573e03f9cb8fef73dd342855a5da7e51
Tom Hoen <tom@givecorps.com>
2020-03-16 11:58:52 -0400

    fixes spec to ensure that the url generator works for both single and mult-level onboarders

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   namespace :onboarding do
+    namespace :create_campaign do
+      resources :recruit_fundraisers, controller: :recruit_fundraisers
+    end
     resources :create_project, controller: :create_project
   end
 end
--- spec/services/onboarding/url_generator_spec.rb
+++ spec/services/onboarding/url_generator_spec.rb
@@ -6,25 +6,27 @@ describe NfgOnboarder::UrlGenerator do
   let(:admin) { create(:admin) }
   let(:context) { ActionController::Base.new.view_context }
 
-  let!(:onboarding_session) do
-    create(:session,
-           owner: admin,
-           onboarder_prefix: 'create_project',
-           completed_high_level_steps: [],
-           current_high_level_step: 'project_name',
-           current_step: 'wicked_finish',
-           onboarder_progress: {
-           }
-    )
-  end
-
   subject { NfgOnboarder::UrlGenerator.new(onboarding_session, context).call }
 
-  it "should return the path to the wicked finish step of the create project controller" do
-    expect(subject).to eq('/onboarding/create_project/wicked_finish')
+  describe 'when using a multi-level onboarder' do
+    let!(:onboarding_session) do
+      create(:session,
+             owner: admin,
+             onboarder_prefix: 'create_campaign',
+             completed_high_level_steps: [],
+             current_high_level_step: 'recruit_fundraisers',
+             current_step: 'wicked_finish',
+             onboarder_progress: {
+             }
+      )
+    end
+
+    it "should return the path to the wicked finish step of the create project controller" do
+      expect(subject).to eq('/onboarding/create_campaign/recruit_fundraisers/wicked_finish')
+    end
   end
 
-  describe "an initial setup onboarding session on the project name step" do
+  describe "with a single level onboarder" do
     let!(:onboarding_session) do
       create(:session,
              owner: admin,
