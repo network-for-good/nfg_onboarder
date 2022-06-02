4ba1dea068b43978d0639a83a2cb9e290daca9c8
Aravind <aravind@clearstack.io>
2022-06-02 15:47:19 +0530

    [#AIO-964] renaming arguments [skip ci]

--- app/presenters/nfg_onboarder/high_level_navigation_bar_presenter.rb
+++ app/presenters/nfg_onboarder/high_level_navigation_bar_presenter.rb
@@ -6,8 +6,8 @@ module NfgOnboarder
     # Ensure that the href is nil (thus supporting accessibility via the nfg_ui Step)
     # when the step is disabled / unclickable
     # or on the last step, all links should have a nil :href
-    def href(c_step, path = '')
-      h.before_last_visited_point_of_no_return?(c_step) ? nil : path_with_id(c_step)
+    def href(nav_step, path = '')
+      h.before_last_visited_point_of_no_return?(nav_step) ? nil : path_with_id(nav_step)
     end
 
     def points_of_no_return
@@ -22,13 +22,13 @@ module NfgOnboarder
     # for the step nav body text
     # or default to humanizing the
     # step's symbol.
-    def step_body(step)
-      I18n.t(step, scope: h.locale_namespace + [:step_navigations], default: step.to_s.humanize)
+    def step_body(nav_step)
+      I18n.t(nav_step, scope: h.locale_namespace + [:step_navigations], default: nav_step.to_s.humanize)
     end
 
     # Returns a check mark for the last step's icon
-    def step_icon(step)
-      step == all_steps.last ? 'check' : nil
+    def step_icon(nav_step)
+      nav_step == all_steps.last ? 'check' : nil
     end
 
     # Returns an nfg_ui friendly status for the :step component's status trait
@@ -53,10 +53,10 @@ module NfgOnboarder
       model.onboarder_progress.keys.include?(nav_step.to_s)
     end
 
-    def path_with_id(c_step)
-      return nil unless model.onboarder_progress.key?(c_step.to_s)
+    def path_with_id(nav_step)
+      return nil unless model.onboarder_progress.key?(nav_step.to_s)
 
-      "/onboarding/create_campaign/#{c_step}/#{model.onboarder_progress[c_step.to_s][0]}"
+      "/onboarding/create_campaign/#{nav_step}/#{model.onboarder_progress[nav_step.to_s][0]}"
     end
   end
 end
