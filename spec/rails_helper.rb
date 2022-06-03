9008ab67f178d993a9135d635d3e83f7a29a06e2
Aravind <aravind@clearstack.io>
2022-06-03 21:31:32 +0530

    reverts rails_helper changes

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,8 +9,6 @@ ActiveRecord::Migrator.migrations_paths = 'spec/dummy/db/migrate'
 #
 # Load support files
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
-# load factories to avoid 'Factory not registered' error locally
-Dir["#{File.dirname(__FILE__)}/factories/*.rb"].each { |f| require f }
 
 Capybara.register_driver :selenium do |app|
   # profile = Selenium::WebDriver::Firefox::Profile.new
